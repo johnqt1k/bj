271fe92203041978ff7a075eee74e93f4f4cf6cf
johnqt1k <Zehlstrom@gmail.com>
2023-09-18 12:20:14 +0200

    Made the cardsgame, gave diffrent colors, demos. Mixed the cards with math.random

--- bj.js
+++ bj.js
@@ -1,2 +1,16 @@
-let a = "John";
-console.log(a);
+const colors = ["Hearts" , "Spades" , "Clover", "Ruter"];
+const demos = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Knight", "Queen", "King", "Ace"];
+const cards = [];
+
+for (const color of colors) {
+    for (const demo of demos) {
+        cards.push(`${demo} i ${color}`);
+    }
+}
+
+function mixCards (cards) {
+    for (let i = cardslength - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+}
